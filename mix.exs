8af5723f0cb76bea3387d6c0d8758c1d33a410b1
Antoine Duchenet <antoine.duchenet@gmail.com>
2018-03-22 22:55:28 +0100

    Prepare for early release

--- mix.exs
+++ mix.exs
@@ -4,10 +4,29 @@ defmodule GTmetrix.MixProject do
   def project do
     [
       app: :gtmetrix,
+      name: "GTmetrix",
       version: "0.1.0",
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/antoine-duchenet/gtmetrix",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      build_embedded: Mix.env == :prod,
       deps: deps()
+    ]
+  end
+
+  defp description() do
+    "GTmetrix REST API client.",
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "postgrex",
+      maintainers: ["Antoine Duchenet"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/antoine-duchenet/gtmetrix"}
     ]
   end
 
