3bf5722b077584783e3dcc830f90f7546403f374
Antoine Duchenet <antoine.duchenet@gmail.com>
2018-03-22 22:42:11 +0100

    Basic URL test

--- lib/gtmetrix.ex
+++ lib/gtmetrix.ex
@@ -2,4 +2,30 @@ defmodule GTmetrix do
   @moduledoc """
   Documentation for GTmetrix.
   """
+
+  use HTTPoison.Base
+
+  defp process_url(url), do: Application.get_env(:gtmetrix, :endpoint, "https://gtmetrix.com/api/0.1") <> url
+  defp process_request_options(_), do: [hackney: [basic_auth: Application.get_env(:gtmetrix, :basic_auth)]]
+end
+
+defmodule GTmetrix.Test do
+  def start(url) do
+    body = {:form, [url: url]}
+    case GTmetrix.post("/test", body , []) do
+      {:ok, response} -> Poison.decode!(response.body)
+      {_, response} -> response
+    end
+  end
+
+  def fetch_results(%{"test_id" => test_id}) do
+    fetch_results(test_id)
+  end
+
+  def fetch_results(test_id) do
+    case GTmetrix.get("/test/" <> test_id , []) do
+      {:ok, response} -> Poison.decode!(response.body)
+      {_, response} -> response
+    end
+  end
 end
--- test/gtmetrix_test.exs
+++ test/gtmetrix_test.exs
@@ -2,3 +2,23 @@ defmodule GTmetrixTest do
   use ExUnit.Case
   doctest GTmetrix
 end
+
+defmodule GTmetrixTest.Test do
+  use ExUnit.Case
+  doctest GTmetrix
+
+
+  test "Start a test" do
+    %{"credits_left" => credits_left, "poll_state_url" => poll_state_url, "test_id" => test_id} = GTmetrix.Test.start("www.google.fr")
+    assert(is_integer(credits_left)) 
+    assert(is_bitstring(poll_state_url)) 
+    assert(is_bitstring(test_id)) 
+  end
+
+  test "Fetch test results" do
+    test = GTmetrix.Test.start("www.google.fr")
+    GTmetrix.Test.fetch_results(test)
+    %{"test_id" => test_id} = test
+    GTmetrix.Test.fetch_results(test_id)
+  end
+end
