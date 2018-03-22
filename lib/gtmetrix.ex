5d1f0b919980f884be8a522a2c21b5eefc4d45e5
Antoine Duchenet <antoine.duchenet@gmail.com>
2018-03-22 21:06:37 +0100

    Design choices basics

--- lib/gtmetrix.ex
+++ lib/gtmetrix.ex
@@ -2,17 +2,4 @@ defmodule GTmetrix do
   @moduledoc """
   Documentation for GTmetrix.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GTmetrix.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/gtmetrix_test.exs
+++ test/gtmetrix_test.exs
@@ -1,8 +1,4 @@
 defmodule GTmetrixTest do
   use ExUnit.Case
   doctest GTmetrix
-
-  test "greets the world" do
-    assert GTmetrix.hello() == :world
-  end
 end
