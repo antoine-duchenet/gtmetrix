29660502857cf4868b90f902ff00740be3321dd1
Antoine Duchenet <antoine.duchenet@gmail.com>
2018-04-14 10:38:08 +0200

    File structure

--- lib/gtmetrix.ex
+++ lib/gtmetrix.ex
@@ -11,25 +11,3 @@ defmodule GTmetrix do
   defp process_request_options(_),
     do: [hackney: [basic_auth: Application.get_env(:gtmetrix, :basic_auth)]]
 end
-
-defmodule GTmetrix.Test do
-  def start(url) do
-    body = {:form, [url: url]}
-
-    case GTmetrix.post("/test", body, []) do
-      {:ok, response} -> Poison.decode!(response.body)
-      {_, response} -> response
-    end
-  end
-
-  def fetch_results(%{"test_id" => test_id}) do
-    fetch_results(test_id)
-  end
-
-  def fetch_results(test_id) do
-    case GTmetrix.get("/test/" <> test_id, []) do
-      {:ok, response} -> Poison.decode!(response.body)
-      {_, response} -> response
-    end
-  end
-end
---
+++ lib/gtmetrix/test.ex
@@ -0,0 +1,21 @@
+defmodule GTmetrix.Test do
+  def start(url) do
+    body = {:form, [url: url]}
+
+    case GTmetrix.post("/test", body, []) do
+      {:ok, response} -> Poison.decode!(response.body)
+      {_, response} -> response
+    end
+  end
+
+  def fetch_results(%{"test_id" => test_id}) do
+    fetch_results(test_id)
+  end
+
+  def fetch_results(test_id) do
+    case GTmetrix.get("/test/" <> test_id, []) do
+      {:ok, response} -> Poison.decode!(response.body)
+      {_, response} -> response
+    end
+  end
+end
